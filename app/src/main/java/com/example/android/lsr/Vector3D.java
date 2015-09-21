fcc85287a84481143490c32c9eb2fc13c4e34642
Adrian Connolly <amjconnolly@gmail.com>
2015-09-21 16:13:13 +0900

    add bare bones 3D vector

---
+++ app/src/main/java/com/example/android/lsr/Vector3D.java
@@ -0,0 +1,25 @@
+package com.example.android.lsr;
+
+/**
+ * Created by adrian on 16/09/15.
+ */
+public class Vector3D {
+
+    //Constants
+    public final static Vector3D NORTH = new Vector3D(0, -1, 0);
+    public final static Vector3D EAST = new Vector3D(1, 0, 0);
+    public final static Vector3D SOUTH = new Vector3D(0, 1, 0);
+    public final static Vector3D WEST = new Vector3D(-1, 0, 0);
+    public final static Vector3D UP = new Vector3D(0, 0, 1);
+    public final static Vector3D DOWN = new Vector3D(0, 0, -1);
+    public final static Vector3D ZERO = new Vector3D(0, 0, 0);
+
+    //members
+    double i, j ,k;
+
+    public Vector3D(double i, double j, double k){
+        this.i=i;
+        this.j=j;
+        this.k=k;
+    }
+}
